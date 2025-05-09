d9b5b5430b4d50577e326cc1a1cc35cb4d45154a
Douglas Oliveira <douglaspo_97@outlook.com>
2025-05-09 15:48:56 -0300

    feat: config s3 bucket using @aws-sdk/client-s3 to store the files that are uploaded

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
+import crypto from "node:crypto";
 import parser from "lambda-multipart-parser";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 import { response } from './utils/response.js';
 
 export async function handler(event) {
   const { files: [file] } = await parser.parse(event);
 
-  if (!file) {
+  if (!file || file.fieldname !== "file") {
     return response(400, {
       error: "File is required"
     })
   }
 
-  return response(200, {})
+  if (file.contentType !== "image/png") {
+    return response(400, {
+      error: "Only png files are accepted."
+    })
+  }
+
+  const s3Client = new S3Client();
+  const commad = PutObjectCommand({
+    Bucket: 'YOUR_BUCKET_NAME',
+    Key: `uploads/${crypto.randomUUID()}-${file.filename}`,
+    Body: file.content
+  });
+
+  const s3Response = await s3Client.send(command);
+
+  return response(200, s3Response.body)
 }
