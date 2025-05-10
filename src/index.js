5ebd873109712a0b85785a65ef0757bcde473877
Douglas Oliveira <douglaspo_97@outlook.com>
2025-05-10 18:29:26 -0300

    fix: ortography errors and add comments

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { response } from './utils/response.js';
 
 export async function handler(event) {
-  const { files: [file] } = await parser.parse(event);
+  const { files } = await parser.parse(event);
+
+  const [file] = files
 
   if (!file || file.fieldname !== "file") {
     return response(400, {
@@ -19,14 +21,24 @@ export async function handler(event) {
     })
   }
 
-  const s3Client = new S3Client();
-  const commad = PutObjectCommand({
+  const s3Client = new S3Client({
+    // You need declare the region of your S3 bucket
+    // if your lambda function is running in a different region
+    // region: 'us-east-2'
+  });
+
+  // Your lambda need permission (role/s3/pubObject)
+  // to store the file on the bucket
+  const command = new PutObjectCommand({
     Bucket: 'YOUR_BUCKET_NAME',
     Key: `uploads/${crypto.randomUUID()}-${file.filename}`,
     Body: file.content
   });
 
-  const s3Response = await s3Client.send(command);
+  await s3Client.send(command);
 
-  return response(200, s3Response.body)
+  return {
+    statusCode: 204,
+    body: null
+  };
 }
