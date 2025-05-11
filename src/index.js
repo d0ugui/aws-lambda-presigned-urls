1d6bec83ce16b368b0a0ec97098345934022af41
Douglas Oliveira <douglaspo_97@outlook.com>
2025-05-11 19:02:54 -0300

    feat: implements presigned-url approach with @aws-sdk/s3-request-presigner

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import crypto from "node:crypto";
-import parser from "lambda-multipart-parser";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { response } from './utils/response.js';
 
 export async function handler(event) {
-  const { files } = await parser.parse(event);
+  const { filename } = JSON.parse(event.body);
 
-  const [file] = files
-
-  if (!file || file.fieldname !== "file") {
+  if (!filename) {
     return response(400, {
-      error: "File is required"
-    })
-  }
-
-  if (file.contentType !== "image/png") {
-    return response(400, {
-      error: "Only png files are accepted."
+      error: "Filename is required"
     })
   }
 
@@ -31,14 +22,10 @@ export async function handler(event) {
   // to store the file on the bucket
   const command = new PutObjectCommand({
     Bucket: 'YOUR_BUCKET_NAME',
-    Key: `uploads/${crypto.randomUUID()}-${file.filename}`,
-    Body: file.content
+    Key: `uploads/${crypto.randomUUID()}-${filename}`,
   });
 
-  await s3Client.send(command);
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 120 });
 
-  return {
-    statusCode: 204,
-    body: null
-  };
+  return response(200, { url });
 }
